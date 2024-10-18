998368056f84da687f0f3f3efd90859127abdf75
Prashanth <ch.prashanthchowdary007@gmail.com>
2024-10-18 22:18:39 +0530

    cheking endpoints

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.send("hello there server is running");
+    res.send("hello is home route");
 })
 
 app.listen(3000,()=>{
