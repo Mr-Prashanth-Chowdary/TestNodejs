7cf52daf4c3a619b291553fa7f914ec5e4ed44f7
Prashanth <ch.prashanthchowdary007@gmail.com>
2024-10-18 22:25:07 +0530

    check endpoint

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("hello is home route");
 })
-
+app.get('/login',(req,res)=>{
+    res.send("this is login route");
+})
 app.listen(3000,()=>{
     console.log('the server is running on 5000');
 })
